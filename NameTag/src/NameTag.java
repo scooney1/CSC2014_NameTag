12148c07308279e28f403dc09c50d52ff3efbfac
scooney1 <36171872+scooney1@users.noreply.github.com>
2018-03-07 13:30:52 -0500

    Add files via upload

---
+++ NameTag/src/NameTag.java
@@ -0,0 +1,18 @@
+/**
+ * Author:	Sam Cooney
+ * Project:	NameTag
+ * Date:	Nov 8, 2017
+ * I assert that the program I am submitting is the result of my own efforts.
+ * General: 
+ */
+
+public class NameTag {
+
+	public static void main(String[] args) {
+		
+		System.out.println();
+		System.out.println("      " + args[0]);
+		System.out.println("My name is " + args[1]);
+	}
+
+}
